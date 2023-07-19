3cd6f652b8a72b2edb281c82163d4df0610ded28
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-07-19 16:14:47 +0200

    more tests (non functional)

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -2,7 +2,20 @@ import numpy as np
 import pandas as pd
 import anndata
 import scanpy as sc
-from sctk import calculate_qc, generate_qc_clusters
+from sctk import (
+    auto_filter_cells,
+    auto_zoom_in,
+    calculate_qc,
+    crossmap,
+    custom_pipeline,
+    find_good_qc_cluster,
+    generate_qc_clusters,
+    get_good_sized_batch,
+    integrate,
+    recluster_subset,
+    simple_default_pipeline,
+)
+from sctk._pipeline import _scale_factor, fit_gaussian
 
 
 def test_calculate_qc():
@@ -39,3 +52,21 @@ def test_generate_qc_clusters():
     generate_qc_clusters(adata, ["n_counts", "n_genes"])
     assert "qc_cluster" in adata.obs.columns
     assert "X_umap_qc" in adata.obsm.keys()
+
+
+def test_scale_factor():
+    # test that _scale_factor returns expected values
+    assert np.isclose(_scale_factor(np.array([0, 1, 2, 3, 4])), 1.0)
+    assert np.isclose(_scale_factor(np.array([0, 1, 2, 3, 10])), 0.5)
+    assert np.isclose(_scale_factor(np.array([0, 1, 2, 3, -10])), -0.5)
+
+
+def test_fit_gaussian():
+    # create test data
+    x = np.random.normal(loc=5, scale=2, size=1000)
+
+    # test that fit_gaussian returns expected values
+    x_peak, x_left, x_right = fit_gaussian(x, n=2, threshold=0.05, plot=False)
+    assert np.isclose(x_peak, 5.0, rtol=0.1)
+    assert np.isclose(x_left, 1.0, rtol=0.1)
+    assert np.isclose(x_right, 9.0, rtol=0.1)
