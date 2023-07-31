3e6ff7c0b69bdec774af43fbb4724635fa3bc0f2
Sebastian Lobentanzer <sebastian.lobentanzer@gmail.com>
2023-07-31 18:32:01 +0200

    initialise sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "sctk"
+copyright = "2023, Teichmann lab"
+author = "nh3, Sebastian Lobentanzer, Krzysztof Polanski"
+release = "0.2.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",  # not for output but to remove warnings
+    "sphinxext.opengraph",
+    "myst_parser",  # markdown support
+    "sphinx_rtd_theme",
+    "sphinx_design",
+]
+myst_enable_extensions = ["colon_fence"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+root_doc = "index"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_title = "sctk"
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    "navigation_depth": 2,
+    "collapse_navigation": True,
+}
---
+++ docs/index.rst
@@ -0,0 +1,17 @@
+Single Cell analysis Tool Kit
+=============================
+
+SCTK is a collection of tools for single cell analysis.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
