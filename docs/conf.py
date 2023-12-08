20d782f016bda5e5fec337e1b25177a87cbb7d9a
ktpolanski <krzysztof_polanski@o2.pl>
2023-12-08 15:01:47 +0000

    Bump version: 0.2.1 → 0.2.2

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "sctk"
 copyright = "2023, Teichmann lab"
 author = "nh3, Sebastian Lobentanzer, Krzysztof Polanski"
-release = "0.2.1"
+release = "0.2.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
