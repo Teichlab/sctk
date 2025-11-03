8b871e1dc5c7433be195058d2075d63a02e3cd60
ktpolanski <krzysztof_polanski@o2.pl>
2025-11-03 23:35:16 +0000

    try nbsphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ extensions = [
     "sphinx.ext.todo",  # not for output but to remove warnings
     "sphinxext.opengraph",
     "sphinx.ext.autosummary",
-    "myst_nb",
+    "nbsphinx",
     "myst_parser",  # markdown support
     "sphinx_rtd_theme",
     "sphinx_design",
